b41acd5cf1dee29207938a0592002fd0dd46656f
a.karavida <a.karavida@efiport.rs>
2021-12-23 19:47:04 +0100

    fix routes

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import { healthCheckRoute } from './routes/healthCheck';
-import { protectedSolarRoute, publicSolarRoute } from './routes/solarRoute';
+import express, { json } from "express";
+import cors from "cors";
+import { healthCheckRoute } from "./routes/healthCheck";
+import { currentSolarRoute, historySolarRoute } from "./routes/solarRoute";
 
 const app = express();
 app.use(json());
 
 app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        optionsSuccessStatus: 200,
-    })
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
 );
 
-app.use('/api/health-check', healthCheckRoute);
-app.use('/api/solar', publicSolarRoute);
-app.use('/api/solar', protectedSolarRoute);
+app.use("/api/health-check", healthCheckRoute);
+app.use("/api/solar/current", currentSolarRoute);
+app.use("/api/solar/history", historySolarRoute);
 
 export { app };
--- src/routes/solarRoute.ts
+++ src/routes/solarRoute.ts
@@ -1,14 +1,16 @@
-import { Router, Request, Response } from 'express';
-import { verifyAccess } from '../middlewares/verifyAccess';
+import { Router, Request, Response } from "express";
+import { verifyAccess } from "../middlewares/verifyAccess";
 
-export const publicSolarRoute = Router().get('/', (req: Request, res: Response) => {
+export const currentSolarRoute = Router().get(
+  "/",
+  (req: Request, res: Response) => {
     res.send({});
-});
+  }
+);
 
-export const protectedSolarRoute = Router().get(
-    '/protected',
-    verifyAccess,
-    (req: Request, res: Response) => {
-        res.send({});
-    }
+export const historySolarRoute = Router().get(
+  "/",
+  (req: Request, res: Response) => {
+    res.send({});
+  }
 );

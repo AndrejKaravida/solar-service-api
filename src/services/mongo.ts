de4c7ae521502c2bcc75429cfa312ee6dd73b253
a.karavida <a.karavida@efiport.rs>
2021-12-23 19:47:00 +0100

    fix routes

---
+++ src/services/mongo.ts
@@ -0,0 +1,15 @@
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://<username>:<password>@energyweathercluster.lkam4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
